2471620ac5eb8165c095372cafbdd17112cca65c
dir-FY24kasaihikari <hikari.kasai.k885@dirbato.co.jp>
2024-04-15 14:06:11 +0900

    Lesson11 終了　修正

--- techacademy_orm/src/util/DBUtil.java
+++ techacademy_orm/src/util/DBUtil.java
@@ -1,7 +1,5 @@
 package util;
 
-import java.io.ObjectInputStream.GetField;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
